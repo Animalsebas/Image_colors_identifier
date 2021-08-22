92eccca6f43d6c6cd4b545521e83bf201fb340ff
animalsebas <animalsebas.kawaii@gmail.com>
2021-08-22 13:02:49 -0500

    v2.1 Win_Build

--- Source Code/ici_win.py
+++ Source Code/ici_win.py
@@ -2,11 +2,12 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import ImageTk, Image
 from scipy.spatial import KDTree
-from webcolors import (
-    css3_hex_to_names,
-    hex_to_rgb,
-)
+from webcolors import (css3_hex_to_names, hex_to_rgb)
+import sys
+import os
+#Loading the image
 def load_image():
+    #Get results in HEX
     def results_hex():
         img = (Image.open(image_dir)).resize((10,10), Image.ANTIALIAS)
         img.convert('RGB')
@@ -40,6 +41,7 @@ def load_image():
         listbox.config(yscrollcommand = scrollbar.set)
         scrollbar.config(command = listbox.yview)
         resultsHEX_window.mainloop()
+    #Get results in RGB
     def results_rgb():
         img = (Image.open(image_dir)).resize((10,10), Image.ANTIALIAS)
         img.convert('RGB')
@@ -73,6 +75,7 @@ def load_image():
         listbox.config(yscrollcommand = scrollbar.set)
         scrollbar.config(command = listbox.yview)
         resultsHEX_window.mainloop()
+    #Get results in CSS colors
     def results_webcolors():
         img = (Image.open(image_dir)).resize((10,10), Image.ANTIALIAS)
         img.convert('RGB')
@@ -120,6 +123,9 @@ def load_image():
         listbox.config(yscrollcommand = scrollbar.set)
         scrollbar.config(command = listbox.yview)
         resultsHEX_window.mainloop()
+    #Exict Function
+    def exit():
+        sys.exit()
     image_dir = filedialog.askopenfilename()
     img= (Image.open(image_dir))
     resized_image= img.resize((300,400), Image.ANTIALIAS)
@@ -134,11 +140,17 @@ def load_image():
     rgb_button.pack(pady=15)
     webcolors_button = Button(aci_window, width=25, height=2, bg = "white", fg = "black", text="WEB COLORS CSS3 Results", font="Arial 14", command = results_webcolors)
     webcolors_button.pack(pady=15)
+#Main window
 aci_window = Tk()
 aci_window.resizable(True, True)
 aci_window.geometry("500x900")
 aci_window.title("Image Colors Identifier")
 aci_window.configure(bg = "gray8")
-image_button = Button(aci_window, width=25, height=2, bg = "white", fg = "black", text="Load image", font="Arial 14", command = load_image)
+#---ATTENTION---
+aci_window.iconbitmap((os.path.join(sys.path[0], "windowIcon.ico"))) #Not working on Pyinstaller Windows Builds
+#---ATTENTION---
+image_button = Button(aci_window, width=25, height=2, bg = "white", fg = "black", text="Load image (.JPG)", font="Arial 14", command = load_image)
 image_button.pack(pady=15)
+autor_label = Label(aci_window, width=25, height = 2, bg = "white", fg = "black", text="Baskerville Inc. SH. v2.1")
+autor_label.pack(pady=15)
 aci_window.mainloop()
